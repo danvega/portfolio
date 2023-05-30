28b5a2d7f8ad5c4582ff3e2e0d46b7617c035a03
Dan Vega <danvega@gmail.com>
2023-05-30 09:22:11 -0400

    Updating projects page to handle null viewer

--- pages/projects.vue
+++ pages/projects.vue
@@ -28,13 +28,15 @@ const query = gql`
 // = ref({ limit: 5})
 const variables = { limit: 5 }
 const { data } = await useAsyncQuery(query, variables);
+
+console.log(data);
 </script>
 
 <template>
   <h1 class="text-3xl my-8">Projects</h1>
   <p class="text-lg mb-8">Here are some of my projects on GitHub.</p>
   <section class="grid grid-cols-2 gap-10">
-    <div v-for="project in data.viewer.repositories.nodes" :key="project.id"
+    <div v-for="project in data?.viewer.repositories.nodes" :key="project.id"
       class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
       <a :href="project.url" target="_blank">
         <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">{{ project.name }}</h2>
