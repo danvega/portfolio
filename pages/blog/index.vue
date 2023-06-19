4a9a3a6472bf72b7cb2cdc296439879d138fa241
joba <jobtennis21@gmail.com>
2023-06-19 16:20:31 +0300

    adding some hover animation and change navs to NuxtLink

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,13 +5,17 @@ const { data: posts } = await useAsyncData('posts', () =>
 </script>
 
 <template>
-  <h1 class="text-3xl my-8">Blog Posts</h1>
+  <div class="border pb-8 rounded bg-gray-100 p-6 mt-8">
+  <h1 class="text-3xl my-8 font-bold ">Blog Posts</h1>
+<p class="font-medium max-w-4xl">
 
   I like to write about anything thta I am currently working or something new that interests me. If you would like me to
   write about something or be a guest blogger on your blog please reach out to me on Twitter. If you would like to
   subscribe to an RSS feed you can find it here.
+</p>
 
-  <section class="grid md:grid-cols-3 mt-8 gap-10">
+</div>
+  <section class="grid md:grid-cols-3 mt-8 gap-10 mb-16">
     <Post :posts="posts" />
   </section>
 </template>
